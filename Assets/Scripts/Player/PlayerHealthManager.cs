87f5b48c785a56dadb8aa3e9ff699bcce353ca9b
Taisen Colcher <TaisenColcher@Gmail.com>
2023-06-17 00:03:31 -0400

    Add Public Static Instance Of PlayerHealthManager

--- Assets/Scripts/Player/PlayerHealthManager.cs
+++ Assets/Scripts/Player/PlayerHealthManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerHealthManager : MonoBehaviour
 {
+
+    public static PlayerHealthManager instance;
+
     // Start is called before the first frame update
     void Start()
     {
