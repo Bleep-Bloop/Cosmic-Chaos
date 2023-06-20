de0ff167610ccbe97d31d89186e54d1054e467e7
Taisen Colcher <TaisenColcher@Gmail.com>
2023-06-20 12:42:34 -0400

    Setup Weapon Getters and Lists
    Created two lists, one for all available weapons on a player character, and one for active weapons.
    Created a getter for both that returns a List<WeaponBase>.
    Set allAvailableWeapons, and allActiveWeapons lists in Start().

--- Assets/Scripts/GameplaySystems/UpgradeManager.cs
+++ Assets/Scripts/GameplaySystems/UpgradeManager.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class UpgradeManager : MonoBehaviour
 {
+
+    [Header("Components")]
+    private CharacterBase playerCharacter;
+    [SerializeField] private List<WeaponBase> allAvailableWeapons = new List<WeaponBase>(); // All weapon's attached to CharacterBase.
+    [SerializeField] private List<WeaponBase> allActiveWeapons = new List<WeaponBase>(); // Attached weapons that are active.
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerCharacter = PlayerHealthManager.instance.GetComponent<CharacterBase>();
+
+        // Get attached weapons
+        allAvailableWeapons = GetAllAvailableWeapons();
+        // Get active attached weapons
+        allActiveWeapons = GetAllActiveWeapons();
+
     }
 
     // Update is called once per frame
@@ -15,4 +27,38 @@ public class UpgradeManager : MonoBehaviour
     {
         
     }
+
+    /// <summary>
+    /// Returns List<WeaponBase> of all weapons attached to playerCharacter.
+    /// Note - Includes Inactive.
+    /// </summary>
+    /// <returns>List<WeaponBase> - All attached weapons including Inactive.</WeaponBase></returns>
+    private List<WeaponBase> GetAllAvailableWeapons()
+    {
+        List<WeaponBase> availableWeapons = new List<WeaponBase>();
+        playerCharacter.GetComponentsInChildren<WeaponBase>(true, availableWeapons);
+        return availableWeapons;
+    }
+
+    /// <summary>
+    /// Returns List<WeaponBase> of all active weapons attached to playerCharacter.
+    /// Note - Requires AllAvailableWeapons. ToDo: Separate
+    /// </summary>
+    /// <returns></returns>
+    private List<WeaponBase> GetAllActiveWeapons()
+    {
+        List<WeaponBase> activeWeapons = new List<WeaponBase>();
+
+        List<WeaponBase> attachedWeapons = GetAllAvailableWeapons();
+        foreach (WeaponBase weapon in attachedWeapons)
+        {
+            if (weapon.isActiveAndEnabled)
+            {
+                activeWeapons.Add(weapon);
+            }
+        }
+
+        return activeWeapons;
+    }
+
 }
