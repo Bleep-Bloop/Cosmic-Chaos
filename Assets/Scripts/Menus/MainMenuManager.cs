712dd2768921910d7fa57e3c1a7669089dadaf4e
Taisen Colcher <TaisenColcher@Gmail.com>
2023-06-21 06:45:22 -0400

    Implement StartDailyChallenge Weekday Find
    StartDailyChallenge() checks the day of the week and then prints a line about what scene it should load.

--- Assets/Scripts/Menus/MainMenuManager.cs
+++ Assets/Scripts/Menus/MainMenuManager.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+
 
 public class MainMenuManager : MonoBehaviour
 {
@@ -12,6 +14,11 @@ public class MainMenuManager : MonoBehaviour
     [SerializeField] private Canvas mainMenuCanvas;
     [SerializeField] private Button startSurvivalGameButton;
     [SerializeField] private Button startDailyChallengeButton;
+
+    private void Awake()
+    {
+
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -33,7 +40,35 @@ public class MainMenuManager : MonoBehaviour
 
     private void StartDailyChallenge()
     {
-        Debug.Log("Find Day of Week and start level");
+        DayOfWeek weekday = DateTime.Today.DayOfWeek;
+        switch (weekday)
+        {
+            case DayOfWeek.Sunday:
+                Debug.Log("Load No Movement Mode");
+                break;
+            case DayOfWeek.Monday:
+                Debug.Log("Load Random Upgrades Mode");
+                break;
+            case DayOfWeek.Tuesday:
+                Debug.Log("Load Instant Kill Mode");
+                break;
+            case DayOfWeek.Wednesday:
+                Debug.Log("Load Instant Max Level");
+                break;
+            case DayOfWeek.Thursday:
+                Debug.Log("Load Downgrade Level");
+                break;
+            case DayOfWeek.Friday:
+                Debug.Log("Load Large Upgrade Value Level");
+                break;
+            case DayOfWeek.Saturday:
+                Debug.Log("Load Still Enemies");
+                break;
+            default:
+                break;
+        }
+
+
     }
 
 }
