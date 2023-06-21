caea6113b725f89228ae70227bb1784433674be9
Taisen Colcher <TaisenColcher@Gmail.com>
2023-06-21 06:33:48 -0400

    Implement MainMenu Button Functionality
    startSurvivalGameButton loads DevScene.
    startDailyChallengeButton prints message.

--- Assets/Scripts/Menus/MainMenuManager.cs
+++ Assets/Scripts/Menus/MainMenuManager.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
+
+    [Header("Components")]
+    [SerializeField] private Canvas mainMenuCanvas;
+    [SerializeField] private Button startSurvivalGameButton;
+    [SerializeField] private Button startDailyChallengeButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startSurvivalGameButton.onClick.AddListener(StartSurvival);
+        startDailyChallengeButton.onClick.AddListener(StartDailyChallenge);
     }
 
     // Update is called once per frame
@@ -15,4 +25,15 @@ public class MainMenuManager : MonoBehaviour
     {
         
     }
+
+    private void StartSurvival()
+    {
+        SceneManager.LoadScene("DevScene");
+    }
+
+    private void StartDailyChallenge()
+    {
+        Debug.Log("Find Day of Week and start level");
+    }
+
 }
